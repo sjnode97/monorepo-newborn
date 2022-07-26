4ff193a5510a6f418278d58ac1331c4b76cb7fa5
1762171403@qq.com <1762171403@qq.com>
2022-07-26 22:01:14 +0800

    init:初始化 前端后端服务以及工具项

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # monorepo-newborn
 An inspirational project and base
+
+## pnpm recursive install
+安装全部依赖
+
+"dev:main-project" 启动前端服务
+
+"dev:main-nodeapi" 启动后端node服务
---
+++ packages/main-project/vite.config.ts
@@ -0,0 +1,44 @@
+// vite.config.ts
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+export default defineConfig({
+  // ...
+  server: {
+    port: 9091,
+    host: "192.168.31.57",
+    proxy: {//跨域
+      // 如果是 /api 打头，则访问地址如下
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      src: "@", // 默认已配置
+      assets: "@/assets",
+      common: "@/common",
+      components: "@/components",
+      api: "@/api",
+      views: "@/views",
+      plugins: "@/plugins",
+      utils: "@/utils",
+    },
+  },
+  plugins: [
+    // ...
+    vue(),
+      // 按需引入vite
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+})
